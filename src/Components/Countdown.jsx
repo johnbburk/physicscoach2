90c0e5dc58a53f2250e05348ab0524f7aacc88e2
John Burk <john.burk98@gmail.com>
2019-02-10 20:08:25 +0100

    replaced countdown.jsx

--- src/Components/Countdown.jsx
+++ src/Components/Countdown.jsx
@@ -1,172 +1,254 @@
-//TODO use this code to adapt login/logout button http://bit.ly/2FVJBcy
-//TODO deal with what happens when you logout in the middle of a session. 
-// Import FirebaseAuth and firebase.
-import React from 'react';
-import ReactDOM from "react-dom";
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
-import App from "./components/App";
-import { BrowserRouter as Router, Route, Redirect, Link, Switch } from "react-router-dom";
-import { logout } from './helpers/auth'
-import UserTest from "./components/UserTest"
-import Main from "./components/Main"
-import AppBar from "./components/layout/AppBar"
-import SimpleDialogDemo from "./components/LoginModal"
-
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+//TODO: style the app with material.ui, also replace add function with doc
+//need to update the style
+//need to refactor so that state is in the app. 
+//idea: create state for local start, and measure splits off of that, then record them in array on FS
 
 
 
-const Public = () => <h3>Public</h3>
-const Protected = () =><h3>Protected</h3>
-const UT = () => <div><UserTest /></div>
+import React, { Component } from "react";
+import "./../styles/index.css";
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid'; 
+import firebase from "../config/constants.js";
+import Modal from "./Modal";
+import StarRatings from 'react-star-ratings'
+
+const db = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+db.settings(settings);
+const sessionsRef = db.collection("sessions")
+//const user = firebase.auth().currentUser;
 
 
 
-class SignInScreen extends React.Component {
+class Countdown extends Component{
   constructor(props){
     super(props);
-  
-  // The component's Local state.
-  this.state = {
-    isSignedIn: false, // Local signed-in state.
-    loading: true,
-    authUser: null
-  };
-  }
-  // Configure FirebaseUI.
-  uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // We will display Google as auth providers.
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false
+    this.state = {
+      sessionTimeEntry: 1, //in min
+      sessionRemainingSeconds: 60, //in seconds
+      running: false,
+      timerLabel: "Session",
+      goal: "",
+      showStart: true,
+      showClose: false,
+      sessionRef: null,
+      rating: 0,
+      goal_comment: "",
+      question_cmmment: "",
+      learn_comment: ""
+
     }
+  
+  this.addSession = this.addSession.bind(this);
+  this.subSession = this.subSession.bind(this);
+  this.startStop = this.startStop.bind(this);
+  this.resetTimer = this.resetTimer.bind(this);
+  this.formatMinutes = this.formatMinutes.bind(this);
+  this.handleChange = this.handleChange.bind(this);
+  this.changeRating = this.changeRating.bind(this);
+  }
+
+  addSession() { //adding and subtracting methods need to also chage the session remaining in seconds to mirrow the entry time if ever changed
+    if (this.state.sessionTimeEntry < 60 && this.state.running === false) {
+      this.setState({
+        sessionTimeEntry: this.state.sessionTimeEntry + 1,
+        sessionRemainingSeconds: (this.state.sessionTimeEntry + 1) * 60
+      })
+    }
+  }
+
+  subSession() {
+    if (this.state.sessionTimeEntry > 1 && this.state.running === false) {
+      this.setState({
+        sessionTimeEntry: this.state.sessionTimeEntry - 1,
+        sessionRemainingSeconds: (this.state.sessionTimeEntry - 1) * 60
+  
+      })
+    }
+  }
+ 
+
+  startStop() {
+    const status = this.state.running;
+    
+    if(!this.state.sessionRef) //first time starting timer, record new database entry
+    {
+      console.log("writing session to cloudstore")
+      const user = firebase.auth().currentUser;
+
+  const sessionRef = sessionsRef.add({
+        start_time: firebase.firestore.FieldValue.serverTimestamp(),
+        user: user.uid,
+        goal: this.state.goal,
+        splits: [] 
+      }).then( ref =>{
+      console.log("Write successful with ID: ", ref.id);
+      this.setState({sessionRef: ref.id});
+      return ref.id;
+      }
+      );
+    }
+
+    //TODO: deal with splits. For now, I'm not recording them in the databse (21/1/19)
+    /* else //need log split in firebase
+    {
+      const sessionRef = this.state.sessionRef
+      console.log("sessionRef is: ",sessionRef)
+      const action = (this.state.running ? "stop":"start");
+      const data = {timestamp: firebase.firestore.FieldValue.serverTimestamp(), action: action}
+      console.log ("data is ",data)
+       const splitRef = sessionsRef.doc(sessionRef).update(
+        {"splits" : firebase.firestore.FieldValue.arrayUnion(
+          data)}
+      ).then(ref=>{
+        console.log("Split write successful with ID ", ref.id);
+        return ref.id
+      }) 
+    } */
+
+    // const chime1 = new Audio("https://res.cloudinary.com/dwut3uz4n/video/upload/v1532362194/352659__foolboymedia__alert-chime-1.mp3") // changed to use <audio> to pass FCC tests
+    switch (status) {
+      case false:
+        console.log("Begin Timer")
+        this.setState({ running: true })
+
+        this.timer = setInterval(() => {
+          if (this.state.running) {
+
+            if  (this.state.sessionRemainingSeconds === 0 ) { 
+              // chime1.play(); // changed to use <audio> to pass FCC tests
+              const sessionRef = this.state.sessionRef
+              console.log(sessionRef)
+              sessionsRef.doc(sessionRef).update({
+                stop_time: firebase.firestore.FieldValue.serverTimestamp()
+              })
+              document.getElementById("notification").play();
+              this.setState({ running: false, showClose: true })
+
+
+            }
+
+
+            if (this.state.sessionRemainingSeconds > 0) {
+              this.setState({
+                sessionRemainingSeconds: this.state.sessionRemainingSeconds - 1,
+                timerLabel: 'Session'
+              });
+            }
+            else {
+              this.setState({
+                timerLabel: 'Session'
+              });
+            }
+          }
+        }, 1000)
+        break;
+      case true:
+        console.log("Stop Timer")
+        this.setState({ running: false })
+        clearInterval(this.timer)
+        break;
+      default:
+        break; 
+    }
+
+  }
+  resetTimer() {
+    this.setState({
+      sessionTimeEntry: 25,
+      sessionRemainingSeconds: 1500,
+      running: false,
+      timerLabel: "Session"
+    })
+  }
+
+  formatMinutes(time) {
+    let seconds = time;
+    const minutes = (seconds % 60 === 0 ) ? ((seconds/60) < 10 ? "0" + seconds/60 : seconds/60): (Math.floor(seconds/60) < 10 ? "0" + Math.floor(seconds/60) : Math.floor(seconds/60));
+    seconds = (seconds % 60 === 0 ) ? "00" : ((seconds % 60 < 10) ? "0"+ (seconds % 60) : seconds % 60)
+    console.log(minutes + ":" + seconds);
+    return minutes + ":" + seconds;
+  }
+
+
+  handleChange(event){
+    this.setState({goal: event.target.goal})
+    }
+
+  goSession = () => {
+     this.setState({ showStart: false });
+     //start timer
+     this.startStop();
   };
 
+  onChange = (event) =>{
+    this.setState({[event.target.name]: event.target.value});
+  };
 
-
-  // Listen to the Firebase Auth state and set the local state.
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(authUser => {
-      authUser ? this.setState({authUser: authUser, isSignedIn: true, loading: false})
-      : this.setState({authUser: null, isSignedIn: false, loading: false})
-    });
-
-
-    // this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-    //     (user) => this.setState({isSignedIn: !!user, loading: false, authUser: user})
-    // );
-    
-  }
+  changeRating = (event)=>{
+    this.setState({rating: event.target.value});
+  };
   
-  // Make sure we un-register Firebase observers when the component unmounts.
-  componentWillUnmount() {
-    this.unregisterAuthObserver();
-  }
-
-  displayLoginModal(){
-    console.log("display LoginModal")
-    ReactDOM.render( <SimpleDialogDemo />, document.getElementById('modal'));
-  }
 
   render() {
-    if (this.state.loading){
-    
-        return (
-          <React.Fragment>
-          <AppBar status = "Login"/>
-          <h1>Loading</h1>
-          </React.Fragment>
-        )
-     
-    }
-      if(!this.state.isSignedIn) {
-          return (
-            <div>
-              <AppBar status = "Login"/>
-              <h1>My App</h1>
-              
-              <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
-          
-              const user = firebase.auth().currentUser;
-
-
-}
-            </div>
-          ) 
-        }
-    return(
-      <React.Fragment>
-      <AppBar status = "Logout" onClick={logout}/>
-      <Router>
-        <div>
-          <ul>
-            <li><Link to ="/public">Public Page</Link></li>
-            <li><Link to ="/protected">Protected</Link></li>
-            <li><Link to ="/user">User</Link></li>
-            <li><Link to ="/newSession">New Session</Link></li>
-            
-        </ul>
-        <Switch>
-          <Route  path="/public" component = {Public}/>
-          <PrivateRoute authed={this.state.isSignedIn} path="/protected" component = {Main}/>
-          <PrivateRoute authed={this.state.isSignedIn} path="/newSession" component = {App}/>
-          <Route  path="/user" component = {UserTest}/>
-        </Switch>          
-          <button
-                        style={{border: 'none', background: 'transparent'}}
-                        onClick={() => {
-                          logout();
-                        }}
-                      >Logout</button>
-
-      
+    return (
+      <div id="clock" >
+      <Grid container spacing = {24}>
+      <Grid item xs={12}>
+        <h1>Pomodoro Clock</h1>
+        <h2>{this.state.goal}</h2>
+        <Modal show = {this.state.showStart} handleClose = {this.goSession} buttonText = "Go!">
+        <form>
+          What is your goal for this session? <input type = "text" name = "goal" onChange={this.onChange}/>
+        </form>
+        <div className="flexContainer">
+          <div id='timerContainer'  className="flexContainer">
+            <h3 id="session-
+            label" className="timerContainerLabels">Session Time</h3>
+            <h3 id="session-length"  className="timerContainerLabels">{this.state.sessionTimeEntry}</h3>
+            <button onClick={this.subSession} id="session-decrement" className="timerContainerButtons">-</button>
+            <button onClick={this.addSession} id="session-increment" className="timerContainerButtons">+</button>
+          </div>
         </div>
-        
-      </Router>
-      </React.Fragment>
+        </Modal>
+        </Grid>
+       
+        <div id="mainTimer">
+          <h1>{this.formatMinutes(this.state.sessionRemainingSeconds)}</h1>
+          <h2>{this.state.timerLabel}</h2>
+         
+          <div id="timerControls" className="flexContainer">
+              <Button variant ="contained" color = "primary" onClick={this.startStop} id="start-stop">Start/Stop</Button>
+              <Button variant = "contained" color = "secondary" onClick={this.resetTimer} id="reset">Reset</Button>
+          </div>
+        </div>
+        </Grid>
+
+      <Modal show = {this.state.showClose}>
+      This is the closing screen.
+      <form>
+          Your Goal: {this.state.goal}
+          Comment on your goal: <input type = "text" name = "goal_comment" onChange={this.onChange}/>
+          Qaulity of practice: <StarRatings rating={this.state.rating} starRatedColor = "red" numberOfStars = {5} name = 'rating' changeRating ={this.changeRating}/>
+          What did you learn: <input type = "text" name = "learn_comment" onChange={this.onChange}/>
+          1 Question you still have: <input type = "text" name = "question_cmmment" onChange={this.onChange}/>
+        </form>
+
+
+      </Modal>
+
+        <audio id="notification" src="https://res.cloudinary.com/dwut3uz4n/video/upload/v1532362194/352659__foolboymedia__alert-chime-1.mp3" preload="auto"></audio> 
+      </div>
     )
-    
+  }
+
+
 }
-}
-
-const PrivateRoute = ({ component: Component, authed, ...rest }) => (
-  <Route {...rest} 
-  render={(props) => (
-    authed === true
-      ? <Component {...props} />
-      : <Redirect to={{
-          pathname: '/login',
-          state: { from: props.location }
-        }} />
-  )} />
-)
-
-const PublicRoute = ({ component: Component, authed, ...rest }) => (
-  <Route {...rest} 
-  render={(props) => (
-    authed === false
-      ? <Component {...props} />
-      : <Redirect to= "/"/>
-  )} />
-)
 
 
-ReactDOM.render(<SignInScreen/>, document.getElementById("root"))
+
+export default Countdown;
+
+
+
