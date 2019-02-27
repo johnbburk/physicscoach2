738da7be884b9bb25aedd6365e9cdbb3fd738aee
jasonxia17 <jasonx3@illinois.edu>
2019-02-27 14:43:25 -0600

    Created a sidebar navigation menu

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,19 +1,50 @@
-import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+
 import LoginMenu from "./LoginMenu";
 import AccountMenu from "./AccountMenu";
-import { Link } from "react-router-dom";
+import SideMenu from "./SideMenu";
 
-export const Header = ({ user }) => (
-  <AppBar position="static">
-    <Toolbar>
-      <Link className="App-logo" to="/">
-        Physics Coach
-      </Link>
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+};
 
-      <div className="Account-container">
-        {user != null ? <AccountMenu /> : <LoginMenu />}
-      </div>
-    </Toolbar>
-  </AppBar>
-);
+function ButtonAppBar(props) {
+  const { classes, user } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+                    
+          {user != null && <SideMenu></SideMenu>}
+          <Typography variant="h5" color="inherit">
+            Physics Coach
+          </Typography>
+
+          <div className={classes.grow}></div>
+          {/* makes the login button right-aligned. */}
+
+          {user != null ? <AccountMenu /> : <LoginMenu />}
+
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export const Header = withStyles(styles)(ButtonAppBar);
