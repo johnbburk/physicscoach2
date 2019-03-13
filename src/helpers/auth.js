dff6a6d66f5b3be5f6264f29540cfd58edfe5dab
John Burk <john.burk98@gmail.com>
2019-03-13 22:39:27 +0100

    added users collection

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -2,13 +2,31 @@ import { ref, firebaseAuth } from "../config/constants";
 import { store } from "../store";
 import history from "../history";
 import { authAction } from "../store/actions";
+import firebase from "../config/constants";
+
+const db = firebase.firestore();
 
 export function fetchUser() {
-  firebaseAuth.onAuthStateChanged(userResult => {
+  firebaseAuth.onAuthStateChanged(async userResult => {
     console.log("fetched user: ", userResult);
     if (userResult === null) {
       history.push("/");
+    } else {
+
+      const user = await db.collection('users').doc(userResult.uid).get();
+      
+      console.log("snapshot exists?", user.exists)
+      if (!user.exists) { // add new users to database
+        db.collection('users').doc(userResult.uid).set({
+          displayName: userResult.displayName,
+          role: "student",
+          email: userResult.email
+        })
+      }
+    
+   
     }
+
     store.dispatch(authAction(userResult));
   });
 }
