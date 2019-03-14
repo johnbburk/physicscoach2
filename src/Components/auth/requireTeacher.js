e9c73a56ffbee4c246e1f80a30f54945d6d74a2f
John Burk <john.burk98@gmail.com>
2019-03-14 02:44:25 +0100

    removed unused imports

--- src/Components/auth/requireTeacher.js
+++ src/Components/auth/requireTeacher.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import history from "../../history";
 
 // higher order component that wraps pages which require authentication
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import firebase from "../config/constants";
 
 const initialState = {
   user: null,
