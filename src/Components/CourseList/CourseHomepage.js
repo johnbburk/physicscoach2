ac8d40263e6f8a52bb6b4c6ea14b4f52a9b1874a
jasonxia17 <jasonx3@illinois.edu>
2019-03-18 00:50:28 -0500

    CourseHomepage checks if user is enrolled

--- src/Components/CourseList/CourseHomepage.js
+++ src/Components/CourseList/CourseHomepage.js
@@ -7,9 +7,20 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { selectCourse } from '../../store';
 
+import firebase from "../../config/constants";
+import { Button } from '@material-ui/core';
+const db = firebase.firestore();
+
 class CourseHomepage extends Component {
-  componentDidMount = () => {
+  state = {
+    courseDoc: null,
+  }
+
+  async componentDidMount() {
     this.props.selectCourse(this.props.match.params.courseID)
+    const courseID = this.props.match.params.courseID;
+    const courseDoc = await db.collection("courses").doc(courseID).get();
+    this.setState({ courseDoc });
   }
 
   render() {
@@ -17,11 +28,33 @@ class CourseHomepage extends Component {
       return <Redirect to="/" />
     }
 
+    if (this.state.courseDoc === null) { // still loading
+      return null;
+    }
+
+    if (!this.state.courseDoc.exists) {
+      return (
+        <div className="Main-content">
+          <h1>This course does not exist.</h1>
+        </div>
+      );
+    }
+
+    if (!this.state.courseDoc.get("students").includes(this.props.user.uid)) {
+      return (
+        <div className="Main-content">
+          <h1>You are not enrolled in this course</h1>
+          <Button color="primary">Request to join</Button>
+        </div>
+      );
+    }
+
     const courseURL = this.props.match.url
 
     const Welcome = () => (
       <div className="Main-content">
-        <h1>Welcome to this course! The ID is: {this.props.match.params.courseID}</h1>
+        <h2>Welcome to {this.state.courseDoc.get("name")}
+            , taught by {this.state.courseDoc.get("teacher")}.</h2>
       </div>
     )
 
