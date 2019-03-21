c6b689c818aa54bb1ec04f197ac3492968425e0e
John Burk <john.burk98@gmail.com>
2019-03-21 20:15:40 +0100

    +safeguard prevents firebase call when no selected

--- src/Components/CourseList/studentList.js
+++ src/Components/CourseList/studentList.js
@@ -23,14 +23,16 @@ class StudentList extends Component {
     const courseDocSnapshot = await this.courseDocRef.get();
     console.log("requests", courseDocSnapshot.get("requests"));
 
-    const listOfIDs = this.props.join ? courseDocSnapshot.get("requests") : courseDocSnapshot.get("students");
+    const listOfIDs = this.props.join
+      ? courseDocSnapshot.get("requests")
+      : courseDocSnapshot.get("students");
 
     let studentRequests = listOfIDs.map(async requestID => {
-        return db
-          .collection("users")
-          .doc(requestID)
-          .get();
-      });
+      return db
+        .collection("users")
+        .doc(requestID)
+        .get();
+    });
 
     studentRequests = await Promise.all(studentRequests);
     studentRequests = studentRequests.map(request => {
@@ -58,35 +60,39 @@ class StudentList extends Component {
       {this.state.studentRequests.map((data, index) => (
         <li key={index}>
           <Checkbox onChange={this.toggleStudent(index)} />
-          <span>{data.displayName + ' (' + data.email + ')'}</span>
+          <span>{data.displayName + " (" + data.email + ")"}</span>
         </li>
       ))}
     </ul>
   );
 
   removeStudents = async () => {
-    const selectedIDs = this.state.studentRequests
-      .filter(student => student.selected)
-      .map(student => student.id);
+      const selectedIDs = this.state.studentRequests
+        .filter(student => student.selected)
+        .map(student => student.id);
 
-    await this.courseDocRef.update({
-      students: firebase.firestore.FieldValue.arrayRemove(...selectedIDs)
-    });
+    if (selectedIDs > 0) {
+      await this.courseDocRef.update({
+        students: firebase.firestore.FieldValue.arrayRemove(...selectedIDs)
+      });
 
-    window.location.reload();
+      window.location.reload();
+    }
   };
 
   addStudents = async () => {
+    
     const selectedIDs = this.state.studentRequests
       .filter(student => student.selected)
       .map(student => student.id);
-
+    
+      if(selectedIDs.length > 0){
     await this.courseDocRef.update({
       requests: firebase.firestore.FieldValue.arrayRemove(...selectedIDs),
       students: firebase.firestore.FieldValue.arrayUnion(...selectedIDs)
     });
-
     window.location.reload();
+  }
   };
 
   render() {
