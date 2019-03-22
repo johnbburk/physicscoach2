843bdfa278978559c67fafd5952612647c29efe9
John Burk <john.burk98@gmail.com>
2019-03-22 22:49:51 +0100

    added students and requests to new FB entries

--- src/Components/CourseList/CreateCourse.js
+++ src/Components/CourseList/CreateCourse.js
@@ -1,5 +1,9 @@
-import React, {Component} from 'react'
-import { createClassDeclaration } from 'typescript';
+//TODO: add check for duplicate course
+//TODO: Style component
+//TODO: Add feature that reports back course url to share with students
+
+import React, { Component } from "react";
+import { createClassDeclaration } from "typescript";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
@@ -7,64 +11,60 @@ import firebase from "../../config/constants";
 
 const db = firebase.firestore();
 
-class CreateCourse extends Component{
-    courseDocRef = db.collection("courses");
-    state = {
-        courseName: "",
-        teacher: ""
+class CreateCourse extends Component {
+  courseDocRef = db.collection("courses");
+  state = {
+    courseName: "",
+    teacher: ""
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  writeNewCourse = () => {
+    if (!this.state.courseName || !this.state.teacher) {
+      return;
     }
 
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-      };
+    this.courseDocRef
+      .add({
+        name: this.state.courseName,
+        teacher: this.state.teacher,
+        students: [],
+        requests: []
+      })
+      .then(ref => {
+        console.log("Write successful with ID: ", ref.id);
+      });
+  };
 
-    writeNewCourse =() =>{
-        if( 
-            !this.state.courseName || 
-            !this.state.teacher
-        ){
-            return;
-        }
+  render() {
+    return (
+      <div className="Main-content">
+        <FormControl>
+          <TextField
+            id="courseName"
+            name="courseName"
+            label="What is the name of your new course?"
+            variant="outlined"
+            onChange={this.onChange}
+          />
 
-        this.courseDocRef.add({
-            name: this.state.courseName,
-            teacher: this.state.teacher
-        })
-        .then(ref => {
-            console.log("Write successful with ID: ", ref.id);
-    })
-    }
-
-
-    render(){
-        return(
-            <div  className="Main-content">
-            <FormControl >
-            <TextField 
-            id = "courseName"
-            name= "courseName"
-            label = "What is the name of your new course?"
-            variant = "outlined"
-            onChange ={this.onChange}
-            />
-           
-            <TextField 
-            id = "teacher"
-            name = "teacher"
-            label = "teacher"
-            variant = "outlined"
-            onChange ={this.onChange}/>
-            
-            </FormControl>
-            <Button color = "primary"
-            onClick = {this.writeNewCourse}
-            >
-            Create Class
-            </Button>
-            </div>
-      )  
-    }
-    
+          <TextField
+            id="teacher"
+            name="teacher"
+            label="teacher"
+            variant="outlined"
+            onChange={this.onChange}
+          />
+        </FormControl>
+        <Button color="primary" onClick={this.writeNewCourse}>
+          Create Class
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default CreateCourse;
