a9f4303a9e1dd9bf192a09ec6b92419eb12162fb
John Burk <john.burk98@gmail.com>
2019-03-23 20:24:26 +0100

    CreateCourse prevents creation of duplicate course

--- src/Components/CourseList/CreateCourse.js
+++ src/Components/CourseList/CreateCourse.js
@@ -1,4 +1,3 @@
-//TODO: add check for duplicate course
 //TODO: Style component
 
 import React, { Component } from "react";
@@ -11,70 +10,84 @@ import firebase from "../../config/constants";
 const db = firebase.firestore();
 
 class CreateCourse extends Component {
-  courseDocRef = db.collection("courses");
-  state = {
-    courseName: "",
-    teacher: firebase.auth().currentUser.displayName,
-    courseID:"",
-    courseURL:"",
-  };
+    courseDocRef = db.collection("courses");
+    state = {
+        courseName: "",
+        teacher: firebase.auth().currentUser.displayName,
+        courseID: "",
+        courseURL: ""
+    };
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
-  writeNewCourse = () => {
-    if (!this.state.courseName) {
-      return;
-    }
-
-    this.courseDocRef
-      .add({
-        name: this.state.courseName,
-        teacher: this.state.teacher,
-        students: [],
-        requests: []
-      })
-      .then(ref => {
-        console.log("Write successful with ID: ", ref.id)
-        if (typeof window !== 'undefined') {
-            const courseURL = window.location.protocol + '//' + window.location.host + "/course/"+ref.id;
-            this.setState({
-                courseID: ref.id,
-                courseURL: courseURL
+    writeNewCourse = async () => {
+        var self = this;
+        if (!this.state.courseName) {
+            return;
+        }
+        console.log("checking name and teacher", this.state.courseName, this.state.teacher)
+        this.courseDocRef
+            .where("name", "==", this.state.courseName)
+            .where("teacher", "==", this.state.teacher)
+            .get()
+            .then(function(doc) {
+                if (doc.empty) {
+                    db.collection("courses")
+                        .add({
+                            name: self.state.courseName,
+                            teacher: self.state.teacher,
+                            students: [],
+                            requests: []
+                        })
+                        .then(ref => {
+                            console.log("Write successful with ID: ", ref.id);
+                            if (typeof window !== "undefined") {
+                                const courseURL =
+                                    window.location.protocol +
+                                    "//" +
+                                    window.location.host +
+                                    "/course/" +
+                                    ref.id;
+                                self.setState({
+                                    courseID: ref.id,
+                                    courseURL: courseURL
+                                });
+                            }
+                        });
+                } //course is a duplicate
+                else {
+                    console.log("duplicate course");
+                    return;
+                }
             });
-        }}
-        )
-  };
+    };
 
-
-  render() {
-
-    return (
-      <div className="Main-content">
-        <FormControl>
-          <TextField
-            id="courseName"
-            name="courseName"
-            label="What is the name of your new course?"
-            variant="outlined"
-            onChange={this.onChange}
-          />
-
-        </FormControl>
-        <Button color="primary" onClick={this.writeNewCourse}>
-          Create Class
-        </Button>
-        <div>
-        {this.state.courseURL ? "Please share this url with your students and ask them to register. \n Your new course url is: ": ""}
-        {this.state.courseURL}
-        </div>
-      
-
-      </div>
-    );
-  
-}
+    render() {
+        return (
+            <div className="Main-content">
+                <FormControl>
+                    <TextField
+                        id="courseName"
+                        name="courseName"
+                        label="What is the name of your new course?"
+                        variant="outlined"
+                        onChange={this.onChange}
+                    />
+                </FormControl>
+                <Button color="primary" onClick={this.writeNewCourse}>
+                    Create Class
+                </Button>
+                <div>
+                    {this.state.courseURL
+                        ? "Please share this url with your students and ask them to register. \n Your new course url is: "
+                        : ""}
+                    {this.state.courseURL}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CreateCourse;
