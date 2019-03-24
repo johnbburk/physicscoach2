1012949ece128b2cbb99ae61ff6c520c645edd6e
John Burk <john.burk98@gmail.com>
2019-03-24 09:52:19 +0100

    revisions to sorting function

--- src/helpers/courseUtils.js
+++ src/helpers/courseUtils.js
@@ -1,24 +1,20 @@
-reverseString = (str) =>{
-    if (str=="")
-    {
-        return "";
-    }
-    else return reverseString(str.substr(1))+str.charAt(0);
-}
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
 
-alphaSort((a,b)=>{
-    const aSplit = a.displayName.split(" ")
-    const bSplit = b.displayName.split(" ")
 
-    if (aSplit.reverse[0] > b.split.reverse[0])
+function alphaSort(a,b){
+    const aSplit = a.split(" ")
+    const bSplit = b.split(" ")
+
+    if (aSplit.reverse[0] > bSplit.reverse[0])
         return 1;
-    if(aSplit.reverse[0] < b.split.reverse[0])
+    if(aSplit.reverse[0] < bSplit.reverse[0])
         return -1;
 
     //how to check to see if there is only one element in the split array? 
     //sort on first name
-    if (aSplit.reverse[1] > b.split.reverse[1])
+    if (aSplit.reverse[1] > bSplit.reverse[1])
         return 1;
-    if(aSplit.reverse[1] < b.split.reverse[1])
+    if(aSplit.reverse[1] < bSplit.reverse[1])
         return -1;
-    else return 0
+    return 0;
+}
