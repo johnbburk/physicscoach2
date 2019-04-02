078aea78c36e02d1d111202490db7cd620be700b
John Burk <john.burk98@gmail.com>
2019-04-02 22:52:10 +0200

    styling changes to PushProtocolButton

--- src/Components/NewSession/PushProtocolButton.js
+++ src/Components/NewSession/PushProtocolButton.js
@@ -58,7 +58,7 @@ const styles = theme => ({
     margin: theme.spacing.unit,
   },
   lightTooltip: {
-    backgroundColor: theme.palette.common.white,
+    backgroundColor: theme.palette.grey[200],
     color: 'rgba(0, 0, 0, 0.87)',
     boxShadow: theme.shadows[1],
     fontSize: 11,
@@ -76,33 +76,6 @@ const styles = theme => ({
       width: 0,
       height: 0,
       borderStyle: 'solid',
-    },
-  },
-  bootstrapPopper: arrowGenerator(theme.palette.common.black),
-  bootstrapTooltip: {
-    backgroundColor: theme.palette.common.black,
-  },
-  bootstrapPlacementLeft: {
-    margin: '0 8px',
-  },
-  bootstrapPlacementRight: {
-    margin: '0 8px',
-  },
-  bootstrapPlacementTop: {
-    margin: '8px 0',
-  },
-  bootstrapPlacementBottom: {
-    margin: '8px 0',
-  },
-  htmlPopper: arrowGenerator('#dadde9'),
-  htmlTooltip: {
-    backgroundColor: '#f5f5f9',
-    color: 'rgba(0, 0, 0, 0.87)',
-    maxWidth: 220,
-    fontSize: theme.typography.pxToRem(12),
-    border: '1px solid #dadde9',
-    '& b': {
-      fontWeight: theme.typography.fontWeightMedium,
     },
   },
 });
@@ -127,32 +100,30 @@ class PushProtocolButton extends Component {
     console.log("elapsed time: ", this.props.elapsedTime);
     return (
       <Tooltip
-        placement="left"
-        classes={{
-          popper: classes.htmlPopper,
-          tooltip: classes.htmlTooltip
-        }}
+        placement="right"
+        title = {<React.Fragment>
+          <Typography color="inherit" variant="h5">
+            If you're stuck...
+          </Typography>
+          <Typography variant = "body1">If you've worked for {PUSH_ACTIVE_TIME} minutes, and you haven't
+          made any progress at all, it's time to activate the push protocol by
+          pressing this button.</Typography>
+          
+          <span className={classes.arrow} ref={this.handleArrowRef} />
+        </React.Fragment>}
+        classes={{ 
+          tooltip: classes.lightTooltip,
+          popper: classes.arrowPopper }}
         PopperProps={{
           popperOptions: {
             modifiers: {
               arrow: {
                 enabled: Boolean(this.state.arrowRef),
-                element: this.state.arrowRef
-              }
-            }
-          }
+                element: this.state.arrowRef,
+              },
+            },
+          },
         }}
-        title={
-          <React.Fragment>
-            <Typography color="inherit" variant="h5">
-              If you're stuck...
-            </Typography>
-            If you've worked for {PUSH_ACTIVE_TIME} minutes, and you haven't
-            made any progress at all, it's time to activate the push protocol by
-            pressing this button.
-            <span className={classes.arrow} ref={this.handleArrowRef} />
-          </React.Fragment>
-        }
       >
         <Button
           style={{ display: "block", margin: "0 auto 20px" }}
