4e6f9fb29f967489c1a523bc08ad4d6ce447728e
John Burk <john.burk98@gmail.com>
2019-04-04 21:15:09 +0200

    added image to welcome.js when not logged in

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -12,6 +12,7 @@ class Welcome extends Component {
       return (
         <div className="Main-content">
           <h1>Welcome to Physics Coach! Please sign in.</h1>
+          <img src="/PhysicsCoach.png" width="800px"/>
         </div>
       )
     }
