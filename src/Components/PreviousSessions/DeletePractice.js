914ce57f047940d24dee9e55203ce10349a7dcb3
John Burk <john.burk98@gmail.com>
2019-04-04 14:05:10 +0200

    removed deleteFirebaseDoc
    This was a one command function that I pulled into DeletePractice

--- src/Components/PreviousSessions/DeletePractice.js
+++ src/Components/PreviousSessions/DeletePractice.js
@@ -2,13 +2,7 @@ import React from "react";
 import { Dialog } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 
-const deleteFirebaseDoc = async (docRef) => {
-  await docRef.delete(),
-
-
-};
-
-const DeletePractice = ({ isOpen, closeDeleteDialog, docRef,reLoad }) => (
+const DeletePractice = async ({ isOpen, closeDeleteDialog, docRef,reLoad }) => (
   <Dialog open={isOpen}>
     Are you sure you want to delete this pracitice?
     <Button size="small" color="primary" onClick={closeDeleteDialog}>
@@ -17,8 +11,8 @@ const DeletePractice = ({ isOpen, closeDeleteDialog, docRef,reLoad }) => (
     <Button
       size="small"
       color="secondary"
-      onClick={() => {
-        deleteFirebaseDoc(docRef);
+      onClick={async () => {
+        await docRef.delete();
         this.props.reLoad()
       }}
     >
