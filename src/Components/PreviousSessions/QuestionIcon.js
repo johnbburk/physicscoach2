7591bf1ab6fbc81575109cdb970f8f976d2517e9
John Burk <john.burk98@gmail.com>
2019-05-13 23:28:06 +0200

    added colors to QuestionIcon.js based on question

--- src/Components/PreviousSessions/QuestionIcon.js
+++ src/Components/PreviousSessions/QuestionIcon.js
@@ -9,20 +9,30 @@ import red from "@material-ui/core/colors/red";
 import green from "@material-ui/core/colors/green";
 
 const styles = {
-  questionIcon: {
+  openQuestionIcon: {
     padding: 4,
-    marginTop: -5
+    marginTop: -5,
+    color: "red"
+  },
+  closedQuestionIcon: {
+    padding: 4,
+    marginTop: -5,
+    color: "green"
   }
 };
 
 class QuestionIcon extends Component {
   render() {
     const { isQuestionOpen, question, classes } = this.props;
+
     if (question) {
       return (
         <IconButton
-          color={isQuestionOpen ? "primary" : "secondary"}
-          className={classes.questionIcon}
+          className={
+            isQuestionOpen
+              ? classes.openQuestionIcon
+              : classes.closedQuestionIcon
+          }
         >
           <Help fontSize="large" />
         </IconButton>
