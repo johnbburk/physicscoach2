f9eb5b3ea5786e2e98caa6ffba4fa173142bf606
John Burk <john.burk98@gmail.com>
2019-05-21 13:10:40 +0200

    setup for firebase deploy

--- src/Components/PreviousSessions/PracticeCard.js
+++ src/Components/PreviousSessions/PracticeCard.js
@@ -4,8 +4,6 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
-import IconButton from "@material-ui/core/IconButton";
-import { Help, Autorenew } from "@material-ui/icons";
 
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
