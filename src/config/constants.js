9e0c0cde1d295bfc2e1f168942e9e907d460aad7
John Burk <john.burk98@gmail.com>
2019-05-22 23:15:45 +0200

    change firebase config

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 const keys = require("./keys");
 
 export const config = {
